a50a95569de4944f9f28192e8dfe0dd038fc4560
Specterr <dragonlava@mail.ru>
2025-05-13 12:29:57 +0300

    Значимое изменение в dev 3

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ class Main {
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке dev");
         System.out.println("change 1");
-        System.out.println("change 1");
+        System.out.println("change 2");
+        System.out.println("change 3");
     }
 }
