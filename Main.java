2d2e3fbdd54182bce70a757e2906b1dfd999efe9
Specterr <dragonlava@mail.ru>
2025-05-13 12:20:40 +0300

    Значимое изменение 2

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ class Main {
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке main");
         System.out.println("aboba 1");
+        System.out.println("aboba 2");
     }
 }
